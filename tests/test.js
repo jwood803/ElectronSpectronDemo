8d071f027157578d4744a66564becf5b90f0d045
James Harrison <james.harrison@intel.com>
2017-04-20 12:49:53 -0700

    trying more combinations of chai.expect

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,7 @@
 const Application = require("spectron").Application;
 const path = require('path');
 const chai = require('chai');
+const should = chai.should();
 const chaiAsPromised = require('chai-as-promised');
 const testPage = require('./test.page.js');
 
@@ -21,29 +22,56 @@ var app = new Application({
         });
 
 global.before(function () {
-    chai.should();
+    //chai.should();
     chai.use(chaiAsPromised);
     page.setApp(app);
 });
 
 describe('Test Example', function () {
-  beforeEach(function (done) {
-      app.start().then(function() { done(); } );
-  });
+    beforeEach(function (done) {
+        app.start().then(function() { done(); } );
+    });
 
-  afterEach(function (done) {
-      app.stop().then(function() { done(); });
-  });
+    afterEach(function (done) {
+        app.stop().then(function() { done(); });
+    });
 
-  it('opens a window', function () {
-    app.client.debug();
+    it('opens a window', function () {
+        app.client.debug();
+        page.getWindowCount().should.eventually.equal(page.windowCount);
+    });
 
-    page.getWindowCount().should.eventually.equal(page.windowCount);
-  });
+    it('yes == no should fail', function () {
+        chai.expect("yes").to.equal("no");
+    });
 
-  it('logs the title', function () {
-      app.client.waitUntilWindowLoaded().getTitle().then(function (txt) { console.warn(txt); });
-  });
+    it('yes == yes should succeed', function () {
+        chai.expect("yes").to.equal("yes");
+    });
+
+    it.only('logs the title', function () {
+        function fn(txt) {
+            var yes = 'yes';
+            //yes.should.be.a('string');
+            yes.should.equal('no');
+            //yes.should.have.lengthOf(2);
+        }
+        ("text")
+    });
+
+    it('logs the title', function () {
+        app.client.waitUntilWindowLoaded().getTitle().then(
+            function (txt) {
+                var yes = 'yes';
+                //yes.should.be.a('string');
+                yes.should.equal('no');
+                //yes.should.have.lengthOf(2);
+            }
+        );
+
+        //app.client.waitUntilWindowLoaded().getTitle().then(function (txt) { console.warn(txt); });
+        //app.client.waitUntilWindowLoaded().getTitle().then(function (txt) { console.warn(typeof txt); });
+    });
 
     it('tests the NOT title', function () {
         app.client.waitUntilWindowLoaded().getTitle().then(function (txt) { chai.expect(txt).to.not.equal("NOT" + page.pageTitle); });
@@ -66,6 +94,6 @@ describe('Test Example', function () {
     });
 
     it('clicks the button', function () {
-    page.clickButtonAndGetText().should.eventually.equal(page.helloText);
-  });
+        page.clickButtonAndGetText().should.eventually.equal(page.helloText);
+    });
 });
